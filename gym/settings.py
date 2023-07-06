d7456d2b5585f4ca6d3e9feb188fab44d2cd1feb
MehranAkbar <mehrankhan6799@gmail.com>
2023-07-06 22:52:03 +0500

    debug false

--- gym/settings.py
+++ gym/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%#*-bkz*@2yp&u_nu3a-ka(h3ol=c4r)awb$a_m@catbp@i#x)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
