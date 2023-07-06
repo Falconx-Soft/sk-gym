34de1ce08206b96ea11cc527505a47fd93e1cc9f
MehranAkbar <mehrankhan6799@gmail.com>
2023-07-06 23:11:03 +0500

    static root 1

--- gym/settings.py
+++ gym/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%#*-bkz*@2yp&u_nu3a-ka(h3ol=c4r)awb$a_m@catbp@i#x)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -111,11 +111,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL = 'images/'
-STATICFILES_DIRS= [
-    BASE_DIR / 'static'
-]
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+MEDIA_URL = 'images/'
 MEDIA_ROOT= os.path.join(BASE_DIR, 'static/images')
 
 
